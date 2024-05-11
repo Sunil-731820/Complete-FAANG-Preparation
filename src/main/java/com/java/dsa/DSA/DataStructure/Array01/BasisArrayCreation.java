cd53e83bb5d9382e16be3e877079f3a30c3b98b9
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2024-05-11 12:53:53 +0530

    Array01Example Is in progress :

---
+++ src/main/java/com/java/dsa/DSA/DataStructure/Array01/BasisArrayCreation.java
@@ -0,0 +1,27 @@
+package com.java.dsa.DSA.DataStructure.Array01;
+
+import java.util.Scanner;
+
+public class BasisArrayCreation {
+	
+	public static void basicArrayCreations(int arr[], int size) {
+		System.out.println("printing the User Array ELement : ");
+		for(int i=0;i<size;i++) {
+			System.out.print(arr[i]+ " ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("This is the EXample of the BasisArrayCreation : ");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Size of The Array by the user");
+		int size = sc.nextInt();
+		int[] arr = new int[size];
+		for(int i=0;i<size;i++) {
+			System.out.println("Enter the ELement In The ARray");
+			arr[i] = sc.nextInt();
+		}
+		basicArrayCreations(arr, size);
+	}
+
+}
