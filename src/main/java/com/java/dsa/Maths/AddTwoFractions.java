14d2dd6d92c12b6744d354e4627cb2635a5824c4
Sunil-731820 <sunilkumargupta1170@gmail.com>
2024-06-07 12:01:55 +0530

    Adding The Two Fractions

---
+++ src/main/java/com/java/dsa/Maths/AddTwoFractions.java
@@ -0,0 +1,26 @@
+package com.java.dsa.Maths;
+
+public class AddTwoFractions {
+	
+//	Methods To add two fractions
+	
+	public static void addTwoFraction(int firstNumberator, int firstDenominator, int secondNumerator,int secondDenominator) {
+		
+		if(firstDenominator==0 && secondDenominator==0) {
+			throw new IllegalArgumentException("Denominator can not Be Zero");
+		}
+		
+		else {
+//			Main Logic Here for adding the two fractions is : 
+			
+		}
+		
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		System.out.println("This is the Example of the adding the two Fractions ");
+	}
+
+}
