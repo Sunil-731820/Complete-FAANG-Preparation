c190800f62cd691eaf9ecad6e3b8d56fd34de74f
Sunil-731820 <sunilg@infinite.com>
2026-03-24 11:53:55 +0530

    Student Recirds data using Array is completed with proper functions

--- src/main/java/com/java/Beg/MainStudentUsingArray.java
+++ src/main/java/com/java/Beg/MainStudentUsingArray.java
@@ -20,6 +20,12 @@ public class MainStudentUsingArray {
         do {
         	System.out.println("========== below is the menu for performing Operations on Student Data====");
         	System.out.println("Enter 1 for Displaying all Students records one by one .");
+        	System.out.println("Enter 2 for Search Student using Student ID ");
+        	System.out.println("Enter 3 for Searcg Student using Student User name :");
+        	System.out.println("Enter 4 for Update Student Marks Using Student id Okay");
+        	System.out.println("Enter 5 for Delete Student using Student Id Okay");
+        	System.out.println("Enter 6 for Sorting the Student Details Using marks");
+        	System.out.println("Enter 7 for Calculating the Average of the marks for the Student Data ");
         	choice = sc.nextInt();
         	switch (choice) {
 			case 1:
@@ -29,6 +35,33 @@ public class MainStudentUsingArray {
 				System.out.println("Enter the Student id :");
 				int id = sc.nextInt();
 				ops.searchByStudentId(id);
+				break;
+				
+			case 3:
+				System.out.println("Enter the Student Name to perform Search operations ");
+				String studentName = sc.next();
+				ops.searchByStudentUsername(studentName);
+				break;
+			case 4:
+				System.out.println("Enter the Student id to update the Student marks ");
+				int studentId = sc.nextInt();
+				System.out.println("Enter the marks of the Student so that we Can update Marks ");
+				double newMarks = sc.nextDouble();
+				ops.updateStudentMarksUsingStudentID(studentId, newMarks);
+				break;
+			case 5:
+				System.out.println("Enter the Student id for Deleting the data ");
+				int deletedStudentId = sc.nextInt();
+				ops.deleteStudentDatausingStudentId(deletedStudentId);
+				break;
+			case 6:
+				System.out.println("Below is the methods of the Sorting The data using Marks :");
+				ops.sortStudentDetailsUsingmarks();
+				break;
+			case 7:
+				System.out.println("below is the methods for calculating the Avarage of the Student marks ");
+				ops.calculateAverageMarksforStudent();
+				break;
 			}
         }while (choice!=0);
     }
--- src/main/java/com/java/Beg/StudentOperations.java
+++ src/main/java/com/java/Beg/StudentOperations.java
@@ -15,7 +15,7 @@ public class StudentOperations {
     
     public void displayAll() {
     	System.out.println("calling the display All Student Details one by one :");
-    	for(int i=0;i<students.length;i++) {
+    	for(int i=0;i<count;i++) {
     		students[i].display();
     	}
     }
@@ -31,8 +31,86 @@ public class StudentOperations {
 	}
 
 	public void searchByStudentId(int id) {
-		// TODO Auto-generated method stub
+		System.out.println("Calling the Search Student using Student Id");
+		for(int i=0;i<students.length;i++) {
+			if(students[i].id==id) {
+				students[i].display();
+				return;
+			}
+		}
+		System.out.println("No Student are with entered Id ");
 		
+		
+	}
+
+	public void searchByStudentUsername(String studentName) {
+		System.out.println("Callling the Search Student using user name :");
+		for(int i=0;i <count;i++) {
+			if(students[i].name.equalsIgnoreCase(studentName)) {
+				students[i].display();
+				return;
+			}
+		}
+		System.out.println("No Students Are found with Given User Name Okay :");
+		
+	}
+
+	public void updateStudentMarksUsingStudentID(int studentId, double newMarks) {
+		System.out.println("Calling the update StudentMarks using Student ID");
+		for(int i=0;i<count;i++) {
+			if(students[i].id ==studentId) {
+				students[i].marks = newMarks;
+				System.out.println("Marks Are updated Successfully Okay ");
+				return;
+			}
+		}
+		System.out.println("No Student Id is matching with Given ID");
+		
+	}
+
+	public void deleteStudentDatausingStudentId(int deletedStudentId) {
+		System.out.println("Calling the Delete Student Data using Student Id ");
+			for(int i=0;i<count;i++) {
+				if(students[i].id ==deletedStudentId) {
+					for(int j=i;j<count-1;j++) {
+						students[j] = students[j+1];
+						students[j].display();
+					}
+					count--;
+					System.out.println("Student Data Are Deleted Successfully from the Array List Okay");
+					return;
+				}
+			}
+			System.out.println("No Student Are found With given Id");
+	}
+
+	public void sortStudentDetailsUsingmarks() {
+        for (int i = 0; i < count - 1; i++) {
+        	System.out.println("Inside the 1 FOR LOOP");
+            for (int j = 0; j < count - i - 1; j++) {
+            	System.out.println("Inside the 2 FOR LOOP..");
+                if (students[j].marks < students[j + 1].marks) {
+                    Student temp = students[j];
+                    students[j] = students[j + 1];
+                    students[j + 1] = temp;
+                    System.out.println("=====AFter Swaping the Value of the Student Details Are ======== ");
+                    System.out.println(students[j].marks);
+                    System.out.println("j+1 Data is :"+students[j+1]);
+                    System.out.println("temp Student data is :"+temp);
+                }
+            }
+        }
+        System.out.println("Sorted by marks (descending):");
+        displayAll();
+    }
+
+	public void calculateAverageMarksforStudent() {
+		System.out.println("calling the calculate Average Marks for all Students ");
+		double sum = 0;
+		for(int i=0;i<count;i++) {
+			sum +=students[i].marks;
+		}
+		System.out.println("the Average of the All Student Marks Are : ="+sum/count);
 	}
     
 }
